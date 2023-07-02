93d508a20b9d4cf990bf7a3288aab555e0c78c78
zoink506 <benhubble03@gmail.com>
2023-07-02 14:08:15 +1000

    Set up associations for Booking and Passenger

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,6 +4,8 @@ class Flight < ApplicationRecord
 
   belongs_to :airport_from, class_name: "Airport"
   belongs_to :airport_to, class_name: "Airport"
+  has_many :bookings
+  has_many :passengers, through: :bookings
 
   def radio_button_value
     "#{self.airport_from.name} -> #{self.airport_to.name} \n #{self.departure_date} \n #{self.duration / 60}"
