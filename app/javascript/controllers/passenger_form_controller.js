0aa07e0c1baf930173d2a9dead53774296c46a2f
zoink506 <benhubble03@gmail.com>
2023-07-15 10:43:30 +1000

    make passenger-form stimulus controller new() duplicate the right template when pressed

--- app/javascript/controllers/passenger_form_controller.js
+++ app/javascript/controllers/passenger_form_controller.js
@@ -22,6 +22,52 @@ export default class extends Controller {
   }
 
   new() {
+    // templates[0].content.querySelector("div.sub-form-div").isEqualNode(divs[0])
+
     console.log("new passenger function");
+    const ediv = document.getElementById("passengers-section");
+    const existingDivs = ediv.querySelectorAll("div.sub-form-div");
+    const templates = ediv.querySelectorAll("template");
+    const templateSection = document.getElementById("templates");
+
+    console.log(existingDivs);
+
+    // ruby pseudocode
+    // divs = ...
+    // templates.each do |template|
+    //   if !divs.any? { |div| div == template }
+    //     divs >> template
+    //     break;
+    //   end
+    // end
+
+    if(existingDivs.length < 4) {
+      const existingDivsArr = Array.from(existingDivs);
+      console.log(existingDivsArr);
+
+      for(let j = 0; j < templates.length; j++) {
+        const template = templates[j];
+
+        console.log(template.content.querySelector("div.sub-form-div"))
+        //existingDivsArr.forEach(div => { console.log(div); console.log(template.content.querySelector("div.sub-form-div").isEqualNode(div)) })
+        let templateFound = false;
+
+        for(let i = 0; i < existingDivs.length; i++) {
+          const div = existingDivs[i];
+
+          console.log(div);
+          console.log(template.content.querySelector("div.sub-form-div").isEqualNode(div))
+          if(template.content.querySelector("div.sub-form-div").isEqualNode(div)) {
+            templateFound = true;
+          }
+        }
+
+        if( templateFound === false ) {
+          templateSection.appendChild(template.content.cloneNode(true));
+          return;
+        }
+      }
+    }
+
   }
 }
